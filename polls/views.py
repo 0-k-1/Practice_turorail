c1246c12daa00cf01c6d349106a1a6c3e57bedd7
geeek0 <geeek0@users.noreply.github.com>
2020-08-27 15:51:45 +0800

    render2

--- polls/views.py
+++ polls/views.py
@@ -20,7 +20,7 @@ def results(request, question_id):
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
-def index(request):
+# def index(request):
     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
@@ -30,9 +30,9 @@ def index(request):
     #     'latest_question_list': latest_question_list,
     # }
     # return HttpResponse(template.render(context, request))
-    def index(request):
-        latest_question_list = Question.objects.order_by('-pub_date')[:5]
-        context = {'latest_question_list': latest_question_list}
-        return render(request, 'polls/index.html', context)
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 # Leave the rest of the views (detail, results, vote) unchanged
